cf110ff24040d787813d1ca2d30c09e174625f48
Yokoi-Taichi <yokoitaichi@outlook.jp>
2021-01-06 02:52:32 +0900

    Create Application.py

---
+++ figmatch/Apprication.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import cv2
+from sklearn.cluster import KMeans
+import PySimpleGUI as sg
+
+import figmatch
+
+LAYOUT = [[sg.Text("Hello World!")]]
+
+def main():
+    """メイン関数"""
+    window = sg.Window("my window", LAYOUT)
+
+    while True:
+        event, value = window.read()
+
+        if event is None:
+            break
+
+    window.close()
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    main()
